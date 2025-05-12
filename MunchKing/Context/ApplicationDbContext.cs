5247e1f1eccedcd7b3a93dc1b149c2fa8a2cfcae
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-05-12 14:34:12 +0200

    Add: ApplicationDbContext with DbSet declarations and model configurations for entities and relationships

---
+++ MunchKing/Context/ApplicationDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MunchKing.Models;
+
+namespace MunchKing.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FoodItem> FoodItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet<ContactMessage> ContactMessages { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Category>()
+                .HasMany(c => c.FoodItems)
+                .WithOne(fi => fi.Category)
+                .HasForeignKey(fi => fi.CategoryId);
+
+            builder.Entity<Order>()
+                .HasMany(o => o.OrderItems)
+                .WithOne(oi => oi.Order)
+                .HasForeignKey(oi => oi.OrderId);
+
+            builder.Entity<Order>()
+                .Property(o => o.Status)
+                .HasConversion<string>(); 
+
+            builder.Entity<FoodItem>()
+                .Property(f => f.Price)
+                .HasPrecision(18, 2);
+
+            builder.Entity<OrderItem>()
+                .Property(oi => oi.UnitPrice)
+                .HasPrecision(18, 2);
+
+            builder.Entity<Order>()
+               .HasOne(o => o.User)
+               .WithMany()
+               .HasForeignKey(o => o.UserId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+
+        }
+
+
+    }
+}
