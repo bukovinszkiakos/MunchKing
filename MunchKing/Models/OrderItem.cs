c2c2b99f596e26cdfdeddcb7697591af11a08772
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-05-12 14:33:57 +0200

    Add: OrderItem model linking orders to food items with quantity and unit price details

---
+++ MunchKing/Models/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿namespace MunchKing.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = default!;
+
+        public int FoodItemId { get; set; }
+        public FoodItem FoodItem { get; set; } = default!;
+
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
