ee8fc5c188dc81475641873b1643a2a188d0bcf6
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-05-12 14:29:45 +0200

    Add: FoodItemRepository with EF Core logic for retrieving, adding, updating, and deleting food items

---
+++ MunchKing/Repositories/IFoodItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using MunchKing.Models;
+
+namespace MunchKing.Repositories
+{
+    public interface IFoodItemRepository
+    {
+        Task<IEnumerable<FoodItem>> GetAllAsync();
+        Task<FoodItem?> GetByIdAsync(int id);
+        Task AddAsync(FoodItem foodItem);
+        void Update(FoodItem foodItem);
+        void Delete(FoodItem foodItem);
+        Task SaveChangesAsync();
+    }
+}
