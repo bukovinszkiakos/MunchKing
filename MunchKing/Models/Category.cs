a277939420fb2e07c741987a00d8f1dfd1797d2b
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-05-12 14:33:14 +0200

    Add: Category model with Id, Name, and navigation property for related FoodItems

---
+++ MunchKing/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace MunchKing.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+
+        public ICollection<FoodItem> FoodItems { get; set; } = new List<FoodItem>();
+    }
+}
