5fe79aab2f0119f01131ba2b238c53bd7d43c439
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-05-12 14:26:40 +0200

    Add: AdminService with methods to retrieve all users with roles and delete a user by ID

---
+++ MunchKing/Services/AdminService.cs
@@ -0,0 +1,49 @@
+﻿using MunchKing.DTOs;
+using Microsoft.AspNetCore.Identity;
+using MunchKing.Models;
+
+namespace MunchKing.Services.Admin
+{
+    public class AdminService : IAdminService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<List<UserDto>> GetAllUsersAsync()
+        {
+            var users = _userManager.Users.ToList();
+            var result = new List<UserDto>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+
+                result.Add(new UserDto
+                {
+                    Id = user.Id,
+                    Username = user.UserName ?? "",
+                    Email = user.Email ?? "",
+                    CreatedAt = user.LockoutEnd?.UtcDateTime ?? DateTime.UtcNow, 
+                    Roles = roles.ToList()
+                });
+            }
+
+            return result;
+        }
+
+        public async Task<bool> DeleteUserAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+                return false;
+
+            var result = await _userManager.DeleteAsync(user);
+            return result.Succeeded;
+        }
+
+    }
+}
---
+++ MunchKing/Services/IAdminService.cs
@@ -0,0 +1,12 @@
+﻿using MunchKing.DTOs;
+
+namespace MunchKing.Services.Admin
+{
+    public interface IAdminService
+    {
+        Task<List<UserDto>> GetAllUsersAsync();
+
+        Task<bool> DeleteUserAsync(string userId);
+
+    }
+}
