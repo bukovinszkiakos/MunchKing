cc3a9a13864d6102dea5fed3593a89ce48f0f92f
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-05-12 14:24:33 +0200

    Add: AdminUserController with endpoint to retrieve all users for admin and super admin roles

---
+++ MunchKing/Controllers/AdminUserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MunchKing.Services;
+
+namespace MunchKing.Controllers
+{
+    [ApiController]
+    [Route("api/admin/users")]
+    [Authorize(Roles = "Admin,SuperAdmin")]
+    public class AdminUserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AdminUserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            return Ok(users);
+        }
+    }
+}
