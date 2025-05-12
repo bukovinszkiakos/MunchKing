0c75d1e1895c6b93dc0c4b5db9a99a9a1a304df6
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-05-12 14:34:34 +0200

    Add: Auth and registration contract records for handling authentication and user signup requests/responses

---
+++ MunchKing/Contracts/AuthResult.cs
@@ -0,0 +1,12 @@
+﻿namespace MunchKing.Contracts
+{
+    public record AuthResult(
+        bool Success,
+        string Email,
+        string Username,
+        string Token
+    )
+    {
+        public Dictionary<string, string> ErrorMessages { get; init; } = new();
+    }
+}
---
+++ MunchKing/Contracts/RegistrationRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MunchKing.Contracts
+{
+    public record RegistrationRequest(
+     [Required] string Email,
+     [Required] string Username,
+     [Required] string Password,
+     [Required] string FullName,
+     [Required] string Address,
+     [Required] string PostalCode,
+     [Required] string MobileNumber,
+     IFormFile? ProfileImage 
+ );
+
+}
