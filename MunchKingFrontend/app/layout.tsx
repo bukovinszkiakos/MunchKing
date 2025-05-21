8e30ea9d4464761a21b8f07b231a0428d5d793c9
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-05-21 18:32:42 +0200

    Add: CartProvider to RootLayout for global cart access and ensure Navbar displays cart quantity

--- MunchKingFrontend/app/layout.tsx
+++ MunchKingFrontend/app/layout.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import "./styles/globals.css";
-import { AuthProvider, useAuth } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
 import Navbar from "./components/Navbar/Navbar";
 import { usePathname } from "next/navigation";
 
 function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-
   const isAdminRoute = pathname.startsWith("/admin");
 
   return (
@@ -23,7 +23,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <AuthProvider>
-          <LayoutWrapper>{children}</LayoutWrapper>
+          <CartProvider>
+            <LayoutWrapper>{children}</LayoutWrapper>
+          </CartProvider>
         </AuthProvider>
       </body>
     </html>
