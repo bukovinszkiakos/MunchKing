7734bb6dc4b9c6c334495a0e99acaf1058371488
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-05-21 18:26:56 +0200

    Add: FullName property and remove default initializers in UserDto

--- MunchKing/DTOs/UserDto.cs
+++ MunchKing/DTOs/UserDto.cs
@@ -2,10 +2,14 @@
 {
     public class UserDto
     {
-        public string Id { get; set; } = default!;
-        public string Username { get; set; } = default!;
-        public string Email { get; set; } = default!;
-        public List<string> Roles { get; set; } = new();
-        public DateTime CreatedAt { get; set; }
+        
+            public string Id { get; set; }
+            public string Username { get; set; }
+            public string FullName { get; set; } 
+            public string Email { get; set; }
+            public List<string> Roles { get; set; }
+            public DateTime CreatedAt { get; set; }
+        
+
     }
 }
