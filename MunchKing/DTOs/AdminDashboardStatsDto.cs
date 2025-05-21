0d643455278608efcc663df9cdd98c980d21b7b2
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-05-21 18:25:53 +0200

    Add: TotalFeedbacks and TotalCategories to AdminDashboardStatsDto

--- MunchKing/DTOs/AdminDashboardStatsDto.cs
+++ MunchKing/DTOs/AdminDashboardStatsDto.cs
@@ -6,6 +6,11 @@
         public int TotalUsers { get; set; }
         public decimal TotalRevenue { get; set; }
         public int TotalFoodItems { get; set; }
+
+        public int TotalFeedbacks { get; set; }
+
+        public int TotalCategories { get; set; }
+
         public Dictionary<string, int> OrdersPerStatus { get; set; } = new();
         public List<TopSellingItemDto> TopSellingItems { get; set; } = new();
     }
