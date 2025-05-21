37c12eb947bf0d3fb3c0f8455ba0fa2ab761f0b5
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-05-21 18:26:24 +0200

    Add: ImageUrl, IsActive, and CreatedAt to CategoryDto

--- MunchKing/DTOs/CategoryDto.cs
+++ MunchKing/DTOs/CategoryDto.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = default!;
+        public string? ImageUrl { get; set; } 
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
