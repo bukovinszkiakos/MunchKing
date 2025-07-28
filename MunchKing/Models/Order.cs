ad4b1407438749f4a7df1e7cbdd74139ee575256
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-07-28 15:39:21 +0200

    Update: Add TotalAmount and DisplayOrderNumber to Order model

--- MunchKing/Models/Order.cs
+++ MunchKing/Models/Order.cs
@@ -9,11 +9,17 @@ namespace MunchKing.Models
         public string UserId { get; set; } = default!;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public OrderStatus Status { get; set; } = OrderStatus.Pending;
-        public string PaymentMode { get; set; } = default!; 
+        public string PaymentMode { get; set; } = default!;
+
+        public int DisplayOrderNumber { get; set; }
+
 
         public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
 
         public ApplicationUser User { get; set; } = default!;
+
+        public decimal TotalAmount { get; set; }
+
     }
 
 }
